2a75d138892a34dacbeeaac2b366f7cbb8cd0c10
bbbbbr <bbbbbr@users.noreply.github.com>
2020-04-24 19:15:38 -0700

    Win32 build: add logging.c to Makefile.am and fix warnings

--- src/logging.c
+++ src/logging.c
@@ -14,10 +14,11 @@ void log_set_level(int new_output_level) {
 
 void log_verbose(const char * format, ...){
 
+    va_list args;
+
     // Only print if vermose mode IS enabled
     if (output_level != OUTPUT_LEVEL_VERBOSE) return;
 
-    va_list args;
     va_start (args, format);
     vprintf (format, args);
     va_end (args);
@@ -25,11 +26,12 @@ void log_verbose(const char * format, ...){
 
 void log_standard(const char * format, ...){
 
+    va_list args;
+
     // Only print if quiet mode and error_only are NOT enabled
     if ((output_level == OUTPUT_LEVEL_QUIET) ||
         (output_level == OUTPUT_LEVEL_ONLY_ERRORS)) return;
 
-    va_list args;
     va_start (args, format);
     vprintf (format, args);
     va_end (args);
@@ -37,10 +39,11 @@ void log_standard(const char * format, ...){
 
 void log_error(const char * format, ...){
 
+    va_list args;
+
     // Only print if quiet mode is NOT enabled
     if (output_level == OUTPUT_LEVEL_QUIET) return;
 
-    va_list args;
     va_start (args, format);
     vprintf (format, args);
     va_end (args);
